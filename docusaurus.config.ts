c022dcaff54a6732842a41e34776df69ba80d966
hlh <hlh@192.168.1.127>
2026-04-03 17:09:52 +0800

    deploy

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -15,7 +15,7 @@ const config: Config = {
   },
 
   // Set the production url of your site here
-  url: 'https://github.com/buffaloboyhu.github.io',
+  url: 'https://github.com/buffaloboyhu',
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
   baseUrl: '/llm-interviews/',
